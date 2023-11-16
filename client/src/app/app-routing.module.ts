45ce03fde28d636f3bd771c502d52423852c101b
Nicovied0 <briannoviedo@gmail.com>
2023-11-16 13:01:17 -0300

    add more components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: "/",
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: "/contact",
+    loadChildren: () => import('./contact/contact.module').then((m) => m.ContactModule)
+  },
+  {
     path: "/dashboard",
     loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
   },
--- client/src/app/contact/contact.module.ts
+++ client/src/app/contact/contact.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
+import { HomeviewComponent } from './view/homeview/homeview.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeviewComponent,
+    LoginFormComponent,
+    RegisterFormComponent
+  ],
   imports: [
     CommonModule,
     ContactRoutingModule
