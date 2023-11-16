05d4002c8322ab7bbfcb3ac71e651686912c621d
Nicovied0 <briannoviedo@gmail.com>
2023-11-16 12:52:30 -0300

    delete view

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeviewComponent } from './view/homeview/homeview.component';
-import { LandingviewComponent } from './view/landingview/landingview.component';
 
 
 @NgModule({
   declarations: [
     HomeviewComponent,
-    LandingviewComponent
   ],
   imports: [
     CommonModule,
