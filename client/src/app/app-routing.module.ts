ce3996d4e4a0641110e8b01a04dfcff4129cf284
Nicovied0 <briannoviedo@gmail.com>
2023-11-16 13:40:28 -0300

    resolve error to routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: "/",
+    path: "",
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
   },
   {
-    path: "/contact",
+    path: "contact",
     loadChildren: () => import('./contact/contact.module').then((m) => m.ContactModule)
   },
   {
-    path: "/dashboard",
+    path: "dashboard",
     loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
   },
 ];
--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeviewComponent } from './view/homeview/homeview.component';
+import { AuthviewComponent } from './view/authview/authview.component';
+import { ProfileviewComponent } from './view/profileview/profileview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeviewComponent
+  },
+  {
+    path: "auth",
+    component: AuthviewComponent
+  },
+  {
+    path: "profile",
+    component: ProfileviewComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
