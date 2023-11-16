0e848637ae9efe1823abd6f7b2b9c5fab23548f3
Nicovied0 <briannoviedo@gmail.com>
2023-11-16 12:44:47 -0300

    add more modules and view

--- client/src/app/vehicles/vehicles.module.ts
+++ client/src/app/vehicles/vehicles.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VehiclesRoutingModule } from './vehicles-routing.module';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
+import { VehicleDetailComponent } from './components/vehicle-detail/vehicle-detail.component';
+import { HomeviewComponent } from './view/homeview/homeview.component';
+import { DetailviewComponent } from './view/detailview/detailview.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VehicleComponent,
+    VehicleDetailComponent,
+    HomeviewComponent,
+    DetailviewComponent
+  ],
   imports: [
     CommonModule,
     VehiclesRoutingModule
