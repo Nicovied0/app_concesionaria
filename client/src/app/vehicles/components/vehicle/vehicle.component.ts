75da19dce961515b3769fa01e9cb49728a969478
Nicovied0 <briannoviedo@gmail.com>
2023-11-17 16:28:31 -0300

    add service getvehicles

--- client/src/app/vehicles/components/vehicle/vehicle.component.ts
+++ client/src/app/vehicles/components/vehicle/vehicle.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VehiclesService } from './../../../../services/Vehicle.service';
 
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
   styleUrls: ['./vehicle.component.scss']
 })
-export class VehicleComponent {
+export class VehicleComponent implements OnInit{
+
+  constructor(private  vehiclesService : VehiclesService){}
+
+  vehicles: any = []
+
+  ngOnInit() {
+    this.getVehicles()
+  }
+
+  getVehicles(){
+      this.vehiclesService.getVehicles().subscribe(
+        res =>{
+          this.vehicles = res
+        }
+      )
+  }
 
 }
---
+++ client/src/services/Vehicle.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class VehiclesService {
+    private linkUrl =  "http://localhost:3001/cars";
+  
+    constructor(private http: HttpClient) { }
+  
+    getVehicles(): Observable<Vehicles[]> {
+      return this.http.get<Vehicles[]>(this.linkUrl).pipe(
+        catchError(() => of([]))
+      );
+    }
+  
+  }
+  
+  export interface Vehicles {
+    active: boolean;
+    brand: string;
+    model: string;
+    year: number;
+    color: string;
+    price: number;
+  }
