18f298811f1432b2c4d6f3862ddfc618dc74046c
Nicovied0 <briannoviedo@gmail.com>
2023-11-28 13:58:03 -0300

    add search service front

---
+++ client/src/services/search.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SearchService {
+
+    constructor(private http: HttpClient) { }
+    private linkUrl =   environment.backUrl +"/search";
+
+    search(consulta: any) {
+        const searchbyname = `${this.linkUrl}?consulta=${consulta}`;
+        return this.http.get<Vehicles[]>(searchbyname).pipe(
+            catchError(() => of([]))
+          );
+    }
+
+    
+}
+
+
+
+
+
+
+export interface Vehicles {
+    active: boolean;
+    brand: string;
+    model: string;
+    year: number;
+    color: string;
+    price: number;
+    dealershipName:string,
+    country:string,
+    state:string,
+    city:string,
+    kilometres:number,
+    _id:String,
+    images:String,
+    condition:String
+    counterVisits:any
+  }
+
+
+
+
+
+
+
