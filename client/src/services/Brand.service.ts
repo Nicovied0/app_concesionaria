0e261663079fa07e856e55c89a7433335fd476fe
Nicovied0 <briannoviedo@gmail.com>
2023-12-11 15:25:04 -0300

    add image and name to brands of cars

--- client/src/services/Brand.service.ts
+++ client/src/services/Brand.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-// Define una interfaz para representar la estructura de los datos de las imágenes
 interface BrandImage {
   id: number;
   brandName: string;
   imageUrl: string;
-  // ... otras propiedades si las hay
 }
 
 @Injectable({
