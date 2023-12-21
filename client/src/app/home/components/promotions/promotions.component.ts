8fa4f88cefcff2b381843b800812b390f031134d
Nicovied0 <briannoviedo@gmail.com>
2023-12-21 11:09:44 -0300

    add promotion component

--- client/src/app/home/components/promotions/promotions.component.ts
+++ client/src/app/home/components/promotions/promotions.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class PromotionsComponent {
 
+  constructor(private router:Router){}
+
+  goVehicle(){
+    this.router.navigate(['/vehicles'])
+    window.scroll(0,0)
+  }
 }
