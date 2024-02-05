e0c11b9eb2a007510a121b603867fe5d2876f4dd
Nicovied0 <briannoviedo@gmail.com>
2024-02-05 17:27:59 -0300

    add deploy environments

--- client/environment.ts
+++ client/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // backUrl: "https://app-concesionaria.vercel.app"
-    backUrl: "http://localhost:3001",
+    backUrl: "https://app-concesionaria.vercel.app",
+    // backUrl: "http://localhost:3001",
     noTechMircoservicesUrl: "https://notech-microservice.vercel.app"
 };
